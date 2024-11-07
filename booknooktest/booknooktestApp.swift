6892419168d1d7b31a7d53a872671c90762e7124
dhirpalshah <dhirpalshah@uchicago.edu>
2024-11-07 14:23:22 -0600

    Initial Commit

---
+++ booknooktest/booknooktestApp.swift
@@ -0,0 +1,17 @@
+//
+//  booknooktestApp.swift
+//  booknooktest
+//
+//  Created by Dhirpal Shah on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct booknooktestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
